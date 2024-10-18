e972ee604b97ddfec1dffdefe983f1ef63e6247f
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-10-18 08:51:59 +0700

    Menambahkan Image Logo pada Layout

--- app/src/main/java/com/example/muzaka2project/MainActivity.kt
+++ app/src/main/java/com/example/muzaka2project/MainActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.muzaka2project.ui.theme.Muzaka2ProjectTheme
 
@@ -41,7 +45,7 @@ fun BasicLayout(modifier: Modifier = Modifier){
     ){
         Text(text = "Login", style = TextStyle(fontSize = 30.sp, fontWeight = FontWeight.Bold))
         Text(text = "Ini adalah halaman login", style = TextStyle(fontSize = 15.sp))
-
+        Image(painter = painterResource(id = R.drawable.logo), contentDescription = null, modifier = Modifier.padding(top = 10.dp).size(290.dp))
     }
 }
 
